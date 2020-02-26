7bba131734ad2415f9f22a0faa5c8ca10f0fd424
ryotas <ryotayamanaka@gmail.com>
2020-02-25 18:10:54 -0800

    first commit

---
+++ docker/server/tmp/extract.sh
@@ -0,0 +1,6 @@
+unzip oracle-graph-zeppelin-interpreter-20.1.0.zip -d ./oracle-graph-zeppelin-interpreter-20.1.0
+cp ./oracle-graph-zeppelin-interpreter-20.1.0/*.jar ../zeppelin/interpreter/pgx/
+rm -r ./oracle-graph-zeppelin-interpreter-20.1.0
+unzip apache-groovy-binary-2.4.18.zip
+cp ./groovy-2.4.18/lib/*.jar ../zeppelin/interpreter/pgx/
+rm -r ./groovy-2.4.18
