d18589e35cbf79103cafdd43bd9eb02c23d9e6b2
Ryota Yamanaka <ryota.yamanaka@oracle.com>
2020-11-23 16:43:18 +0900

    Update extract.sh

--- extract.sh
+++ extract.sh
@@ -7,7 +7,7 @@ rm -rf ./scripts/oracle-graph-plsql
 unzip ./packages/oracle-graph-plsql-$OV.zip -d ./scripts/oracle-graph-plsql
 
 rm -rf ./zeppelin/interpreter/pgx
-mkdir ./zeppelin/interpreter/pgx
+mkdir -p ./zeppelin/interpreter/pgx
 unzip ./packages/oracle-graph-zeppelin-interpreter-$OV.zip -d ./zeppelin/interpreter/pgx/
 unzip ./packages/apache-groovy-binary-$AV.zip -d ./packages/
 cp ./packages/groovy-$AV/lib/*.jar ./zeppelin/interpreter/pgx/
