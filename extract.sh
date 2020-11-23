bed24651aff22c325e097e2236f03f607e10a4d0
Ryota Yamanaka <ryota.yamanaka@oracle.com>
2020-11-23 16:41:54 +0900

    Update extract.sh

--- extract.sh
+++ extract.sh
@@ -6,7 +6,8 @@ AV=2.4.20 # Apache Groovy Version
 rm -rf ./scripts/oracle-graph-plsql
 unzip ./packages/oracle-graph-plsql-$OV.zip -d ./scripts/oracle-graph-plsql
 
-rm ./zeppelin/interpreter/pgx/*
+rm -rf ./zeppelin/interpreter/pgx
+mkdir ./zeppelin/interpreter/pgx
 unzip ./packages/oracle-graph-zeppelin-interpreter-$OV.zip -d ./zeppelin/interpreter/pgx/
 unzip ./packages/apache-groovy-binary-$AV.zip -d ./packages/
 cp ./packages/groovy-$AV/lib/*.jar ./zeppelin/interpreter/pgx/
